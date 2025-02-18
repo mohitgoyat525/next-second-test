3c7b63d699411a2c76bdc7183469559016f858e7
mohitgoyat525 <montyits71@gmail.com>
2025-02-18 12:32:30 +0530

    add-changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Hero from "@/components/Hero";
-import ToDoList from "@/components/ToDoList";
-import Image from "next/image";
+
 import HeroLayout from "./test/[slug]/dashboard/page";
 
 export default function Home() {
